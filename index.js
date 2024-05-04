a7c86f78376e74a8d5e087671bf1230359c3e9fb
mithlesh prasad <101449591+mithleshprasad@users.noreply.github.com>
2024-05-04 18:51:49 +0000

    find missing number in array

--- index.js
+++ index.js
@@ -1,14 +1,29 @@
 const express = require('express');
-
+const cors = require('cors');
 const app = express();
-
 const PORT = 4001;
 
-app.get('/',(req,res) => {
+// Use CORS middleware
+app.use(cors());
+
+app.get('/things', (req, res) => {
     res.send("node express practice");
-})
+});
 
+const array = [1, 3, 4, 5, 6, 7];
+app.get('/', (req, res) => {
+    array.sort((a, b) => a - b);
+    console.warn(array);
+    for (let i = 0; i < array.length; i++) {
+        if (array[i] !== i + 1) {
+            res.send(String(i + 1));
+            return; 
+        }
+    }
+    res.send(String(array.length + 1));
+});
 
+// Start the server
 app.listen(PORT, () => {
-    console.log(`listen port ${PORT}`);
-})
+    console.log(`Listening on port ${PORT}`);
+});
